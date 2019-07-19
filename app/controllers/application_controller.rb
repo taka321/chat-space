fe34d9fdd12459a81e2ab45a7d58b23df30dc177
taka321 <takasaki1232@gmail.com>
2019-07-19 16:07:34 +0900

    add user group

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user! #未ログイン時はログインページに遷移する
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters #追加したキーのパラメーターを許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
